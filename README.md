9d012d0a8e149777eb31233e2ae75b055e49eeec
nkremerh <nkremerh@nd.edu>
2017-09-05 14:57:01 -0400

    Adds UNIX style option handling to grading scripts Modifies readme to reflect option handling changes

--- README.md
+++ README.md
@@ -18,7 +18,13 @@ make setup
 To perform a naive grading of an assignment, use simple_daily_grader.pl:
 
 ```
-perl simple_daily_grader.pl <target directory (default named curr_assignment)> <target assignment name (i.e. regex_primer/homework.sed)> <answer key file> <command to execute the assignment (i.e. "sed -n -E -f")> <other input file (i.e. ~/path/to/input.txt)>
+perl simple_daily_grader.pl --help
+```
+
+There are a few required options for the grader. The --inp option specifies extra input files. Here is an example invocation:
+
+```
+perl simple_daily_grader.pl --dir ./curr_assignment --cmd regex_primer/homework.sed --key ~/path/to/key.txt --cmd "sed -n -E -f" --inp ~/path/to/input.txt
 ```
 
 It will probably be helpful to redirect the output of simple_daily_grader.pl to an output file for easy parsing. The grading script will inform you whether it could find the expected assignment file and whether the generated output from the student's program matches the answer key. The script does not perform any complex matching on the answer key. So if the expected output for an assignment was: 1,2,3,4 but a student's program produced the output: 1 2 3 4, the script would flag that as incorrect even though we can easily tell the output matches the content of the answer key. Additional care for these corner cases is needed. This script is meant as a first pass for grading, not a final evaluation.
--- simple_daily_grader.pl
+++ simple_daily_grader.pl
@@ -2,18 +2,81 @@
 
 use strict; 
 use Scalar::Util qw(looks_like_number);
+use Error qw(:try);
+use Error::Simple;
+use Getopt::Long qw(:config no_ignore_case);
 
 my $num_args = $#ARGV + 1;
-if($num_args != 5) {
-	print "Must specify assignment directory, assignment source file, answer key file, command to execute upon input data file, and any other input file.\nI.E. perl ./simple_daily_grader.pl ./curr_assignment homework.sed ./output.txt \"sed -n -E -f\" input.txt\n";
-	exit 1;
-}
+#if($num_args != 5) {
+#	print "Must specify assignment directory, assignment source file, answer key file, command to execute upon input data file, and any other input file.\nI.E. \n";
+#	exit 1;
+#}
 
-my $dir = $ARGV[0];
-my $src = $ARGV[1];
-my $key = $ARGV[2];
-my $command = $ARGV[3];
-my $input = $ARGV[4];
+my $usage = "Simple Daily Grader Options:
+
+Required:
+	--dir <path>		Sets the directory the grader searches for submissions.
+	--src <path>		Sets the path the grader searches for in each sub-directory.
+	--key <path>		Specify the path to the answer key file.
+	--cmd <string>		Specify the command to execute on each submission.
+	
+Optional:
+	--inp <string>	    Specify a comma separated string of paths to each additional common input file needed.
+
+	--help				Display this message.
+
+Example Usage:
+
+	perl simple_daily_grader.pl --dir ./curr_assignment --src regex_primer/homework.sed --key ~/path/to/key.txt --cmd \"sed -n -E -f\" --inp \"~/path/to/input.txt,~/path/to/input2.txt\"
+
+";
+
+my %OPT;
+try {
+    GetOptions(
+		"dir=s" => \$OPT{dir},
+	   	"src=s" => \$OPT{src},
+	   	"key=s" => \$OPT{key},
+	   	"cmd=s" => \$OPT{cmd},
+	   	"inp=s" => \$OPT{inp},
+	   	"help|?" => sub {print $usage; exit(0)}
+	);
+} 
+catch Error::Simple with {
+    my $E = shift;
+    print STDERR $E->{-text};
+    die "Failed to parse command line options.\n";
+};
+
+
+my $dir = $OPT{dir};
+my $src = $OPT{src};
+my $key = $OPT{key};
+my $cmd = $OPT{cmd};
+my $inp = $OPT{inp};
+
+if(!$dir) { print(STDERR "Missing --dir option.\n"); print_help(); }
+if(!$src) { print(STDERR "Missing --src option.\n"); print_help(); }
+if(!$key) { print(STDERR "Missing --key option.\n"); print_help(); }
+if(!$cmd) { print(STDERR "Missing --cmd option.\n"); print_help(); }
+
+if($inp ne "") {
+	my $err = 0;
+	my @inputs = split(',', $inp);
+	foreach my $input (@inputs) {
+		if($input ne "" && -e "$input") {}
+		elsif($input ne "") {
+			print(STDERR "Extra input file $input does not exist.\n");
+			$err++;
+			next;
+		}
+		else {}
+	}
+	if($err) {
+		print(STDERR "Could not find $err extra input files.\nPlease check the path(s) and try again.\n");
+		exit 1;
+	}	
+}
 
 my @names = <$dir*>;
 
@@ -38,17 +101,10 @@ foreach my $name (@names) {
 		print(STDOUT "File $name/$src does not exist.\n$name (INCORRECT)\n");
 		$wrong = 1;
 		next;
-	}
-	if($input ne "" && -e "$input") {}
-	elsif($input ne "") {
-		print(STDOUT "Extra input file $input does not exist.\n$name (INCORRECT)\n");
-		$wrong = 1;
-		next;
-	}
-	else {}
+	}	
 
-	#my $result = system("dos2unix ./$name/*; $command ./$name/$input > ./$name/output.txt");
-	my $result = system("cd $name && $command $src $input > $src.output");
+	#my $result = system("dos2unix ./$name/*; $cmd ./$name/$input > ./$name/output.txt");
+	my $result = system("cd $name && $cmd $src $inp > $src.output");
 	
 	open(OUTPUT,"$name/$src.output") or die "Could not find output.\n";
 	foreach my $line (<OUTPUT>) {
@@ -81,4 +137,11 @@ foreach my $name (@names) {
 		print "$name (SUCCESS)\n";
 	}
 }
+
 exit 0;
+
+sub print_help {
+
+	print $usage;
+	exit 1;
+}
